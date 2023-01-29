95e7a96d5e657ac82e97e933a763a0cf1801e2b6
FranciszekRyszka <fr.ryszka@gmail.com>
2023-01-29 23:02:36 +0100

    Changed title in index.js file

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>Framer Motion</title>
         <meta name="description" content="Framer Motion Test with Next.JS 13" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
