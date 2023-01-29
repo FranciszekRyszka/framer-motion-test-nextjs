250b1de2f7b30e53ec27ddcc167ce6e85dda979f
FranciszekRyszka <fr.ryszka@gmail.com>
2023-01-29 22:51:45 +0100

    created navbar.tsx file - framer is working here or so I think; in index.js it's still isn't working

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,29 @@
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
-import { motion, useScroll } from "framer-motion";
+import {motion, useScroll, useTransform, useMotionValue, useInView} from "framer-motion";
+import Navbar from "@/pages/navbar";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+    const { scrollY } = useScroll();
+    const y1 = useTransform(scrollY, [0, 300], [0, 200]);
+    const y2 = useTransform(scrollY, [0, 300], [0, -100]);
+
+    const variants = {
+        visible: {
+            opacity: 1,
+            scale: 1,
+            y: 0
+        },
+        hidden: {
+            opacity: 0,
+            scale: 0.65,
+            y: 50
+        }
+    };
+
   return (
     <>
       <Head>
@@ -15,37 +34,40 @@ export default function Home() {
       </Head>
       <main className="bg-slate-900 h-full w-full">
           <section>
+            <Navbar></Navbar>
             <div className="justify-items-center space-y-20">
-                <div  className="bg-sky-400/90 m-auto w-[1300px] h-96 rounded-3xl">
+                <motion.div
+                    transition={{ duration: 2, ease: 'easeOut'}}
+                    className="bg-sky-400/90 m-auto w-[1300px] h-96 rounded-3xl">
                     <div></div>
                     <div></div>
                     <div></div>
                     <div></div>
-                </div>
-                <div  className="bg-sky-400/90 m-auto w-[1300px] h-96 rounded-3xl">
+                </motion.div>
+                <motion.div initial="hidden" whileInView="visible" viewport={{ once: true }}  className="bg-sky-400/90 m-auto w-[1300px] h-96 rounded-3xl">
                     <div></div>
                     <div></div>
                     <div></div>
                     <div></div>
-                </div>
-                <div  className="bg-sky-400/90 m-auto w-[1300px] h-96 rounded-3xl">
+                </motion.div>
+                <motion.div initial="hidden" whileInView="visible" viewport={{ once: true }}  className="bg-sky-400/90 m-auto w-[1300px] h-96 rounded-3xl">
                     <div></div>
                     <div></div>
                     <div></div>
                     <div></div>
-                </div>
-                <div  className="bg-sky-400/90 m-auto w-[1300px] h-96 rounded-3xl">
+                </motion.div>
+                <motion.div initial="hidden" whileInView="visible" viewport={{ once: true }}  className="bg-sky-400/90 m-auto w-[1300px] h-96 rounded-3xl">
                     <div></div>
                     <div></div>
                     <div></div>
                     <div></div>
-                </div>
-                <div  className="bg-sky-400/90 m-auto w-[1300px] h-96 rounded-3xl">
+                </motion.div>
+                <motion.div initial="hidden" whileInView="visible" viewport={{ once: true }}  className="bg-sky-400/90 m-auto w-[1300px] h-96 rounded-3xl">
                     <div></div>
                     <div></div>
                     <div></div>
                     <div></div>
-                </div>
+                </motion.div>
             </div>
           </section>
       </main>
---
+++ src/pages/navbar.tsx
@@ -0,0 +1,72 @@
+import {motion, spring, Variants} from "framer-motion";
+import {useState} from "react";
+
+const itemVariants: Variants = {
+    open: {
+        opacity: 1,
+        y: 0,
+        transition: { type: "spring", stiffness: 300, damping: 24 }
+    },
+    closed: { opacity: 0, y: 20, transition: { duration: 0.2 }}
+};
+
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    return(
+        <motion.nav
+            initial={false}
+            animate={isOpen ? "open" : "closed"}
+            className="m-auto justify-center justify-items-center"
+        >
+            <motion.button
+                whileTap={{ scale: 0.97 }}
+                onClick={() => setIsOpen(!isOpen)}
+                className="text-sky-400 text-2xl bg-sky-700 rounded-xl w-36 h-12 items-space-between"
+            >
+                Menu
+                <motion.div
+                    variants={{
+                        open: { rotate: 180 },
+                        closed: { rotate: 0 }
+                    }}
+                    transition={{ duration: 0.2 }}
+                    style={{ originY: 0.55 }}
+                >
+                    <svg width="15" height="15" viewBox="0 0 20 20">
+                        <path d="M0 7 L 20 7 L 10 16" />
+                    </svg>
+                </motion.div>
+            </motion.button>
+            <motion.ul
+                variants={{
+                    open: {
+                        clipPath: "inset(0% 0% 0% 0% round 10px)",
+                        transition: {
+                            type: "spring",
+                            bounce: 0,
+                            duration: 0.7,
+                            delayChildren: 0.3,
+                            staggerChildren: 0.05
+                        }
+                    },
+                    closed: {
+                        clipPath: "inset(10% 50% 90% 50% round 10px)",
+                        transition: {
+                            type: "spring",
+                            bounce: 0,
+                            duration: 0.3
+                        }
+                    }
+                }}
+                style={{ pointerEvents: isOpen ? "auto" : "none" }}
+            >
+                <motion.li variants={itemVariants} className="text-cyan-400">Item 1</motion.li>
+                <motion.li variants={itemVariants} className="text-cyan-400">Item 2</motion.li>
+                <motion.li variants={itemVariants} className="text-cyan-400">Item 3</motion.li>
+                <motion.li variants={itemVariants} className="text-cyan-400">Item 4</motion.li>
+            </motion.ul>
+        </motion.nav>
+    )
+}
+
+export default Navbar
